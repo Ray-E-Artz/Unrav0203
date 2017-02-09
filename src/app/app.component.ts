1f3fd82437100b578dc18dd75b4a2eb294b6c1e2
Ray-E-Artz <ray.e.artz@gmail.com>
2017-02-09 10:46:11 -0600

    generated add-site component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public dives = [];
+  private index = 0;
+
+  private stockDives = [
+    {
+      site: 'Abu Gotta Ramada',
+      location: 'Hurghada, Egypt',
+      depth: 72,
+      time: 54
+    },
+    {
+      site: 'Ponte Mahoon',
+      location: 'Maehbourg, Mauritius',
+      depth: 54,
+      time: 38
+    },
+    {
+      site: 'Molnar Cave',
+      location: 'Budapest, Hungary',
+      depth: 98,
+      time: 62
+    }];
+
+  public enableAdd() {
+    return this.index < this.stockDives.length;
+  }
+
+  public addDive() {
+    if (this.enableAdd()) {
+      this.dives.push(this.stockDives[this.index++]);
+    }
+  }
+
+  public clearDives() {
+    this.dives = [];
+    this.index = 0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SiteListComponent } from './site-list/site-list.component';
+import { AddSiteComponent } from './add-site/add-site.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteListComponent,
+    AddSiteComponent
   ],
   imports: [
     BrowserModule,
